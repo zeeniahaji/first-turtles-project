d77b868355b22b0713e5d68854445560a7fa60a3
Zeenia <127770650+zeeniahaji@users.noreply.github.com>
2025-03-04 13:11:55 +0000

    Update MoveTest.java

--- uk.ac.kcl.inf.mdd1.turtles.tests/src/uk/ac/kcl/inf/mdd1/tests/move/MoveTest.java
+++ uk.ac.kcl.inf.mdd1.turtles.tests/src/uk/ac/kcl/inf/mdd1/tests/move/MoveTest.java
@@ -6,9 +6,5 @@ import org.junit.runner.RunWith;
 
 @RunWith(XpectRunner.class)
 public class MoveTest extends XtextTests {
-    @org.junit.Test
-    public static void logTestedFile() {
-        String testedFile = "uk.ac.kcl.inf.mdd1.turtles/src/uk/ac/kcl/inf/mdd1/Turtles.xtext";
-        System.out.println("TESTED_FILE: " + testedFile);
-    }
+    
 }
