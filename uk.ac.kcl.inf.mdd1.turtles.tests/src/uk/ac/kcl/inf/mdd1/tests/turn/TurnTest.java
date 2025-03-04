38c58b98c8bf0978bd789fde2e73d65b63fdcd64
Zeenia <127770650+zeeniahaji@users.noreply.github.com>
2025-03-04 12:55:10 +0000

    Update TurnTest.java

--- uk.ac.kcl.inf.mdd1.turtles.tests/src/uk/ac/kcl/inf/mdd1/tests/turn/TurnTest.java
+++ uk.ac.kcl.inf.mdd1.turtles.tests/src/uk/ac/kcl/inf/mdd1/tests/turn/TurnTest.java
@@ -6,5 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(XpectRunner.class)
 public class TurnTest extends XtextTests {
-
+    @org.junit.Test
+    public static void logTestedFile() {
+        String testedFile = "uk.ac.kcl.inf.mdd1.turtles/src/uk/ac/kcl/inf/mdd1/Turtles.xtext";
+        System.out.println("TESTED_FILE: " + testedFile);
+    }
 }
